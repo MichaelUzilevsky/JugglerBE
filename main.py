44ef74604edc62c32be1ee490e050ac353613c74
Michael Uzilevsky <michael.uzilevsky@gmail.com>
2025-07-17 22:51:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,3 @@
-# import asyncio
-#
-#
-# async def main():
-#     """
-#     Main entry point for the application.
-#     """
-#     pass
-#
-#
-# if __name__ == "__main__":
-#     asyncio.run(main())
 import uvicorn
 
 from src.api.app_factory import AppFactory
