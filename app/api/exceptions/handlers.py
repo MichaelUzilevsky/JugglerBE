0061f4ab32491e09436b82c57d1c0aaf0cbdfa66
Michael Uzilevsky <michael.uzilevsky@gmail.com>
2025-09-26 17:43:42 +0300

    Fix rundom casting error.

--- app/api/exceptions/handlers.py
+++ app/api/exceptions/handlers.py
@@ -5,11 +5,21 @@ from app import logger
 from app.domain.exceptions.domain_exception import DomainException
 
 
-async def domain_exception_handler(request: Request, exc: DomainException) -> JSONResponse:
-    logger.error(exc.message, exc_info=True, extra={"event": "domain_exception", "path": str(request.url)})
+async def domain_exception_handler(request: Request, exc: Exception) -> JSONResponse:
+    if isinstance(exc, DomainException):
+        logger.error(exc.message, exc_info=True, extra={
+            "event": "domain_exception",
+            "path": str(request.url)
+        })
+        return JSONResponse(
+            status_code=exc.status_code,
+            content={"detail": exc.message}
+        )
+    # fallback for other exceptions
+    logger.critical(str(exc), exc_info=True, extra={"event": "unhandled_exception", "path": str(request.url)})
     return JSONResponse(
-        status_code=exc.status_code,
-        content={"detail": exc.message}
+        status_code=500,
+        content={"detail": "Internal Server Error"}
     )
 
 
